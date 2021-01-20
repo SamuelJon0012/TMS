c09b714f36911b6133b6f51c99b0b1d383037f33
olsonhost <erik@olson.host>
2021-01-20 21:56:14 +0000

    BurstIQ Models

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,18 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+// Grab the jwt from BurstIq for the current user and store it as a Cookie
+#Route::get('/jwt', 'BurstIqController@jwt')->name('jwt');
+
+Route::get('/biq/status', 'BurstIqController@status')->name('status');
+Route::get('/biq/login', 'BurstIqController@login')->name('login');
+Route::get('/biq/test', 'BurstIqController@test')->name('test');
+Route::get('/biq/test2', 'BurstIqController@test2')->name('test2');
+
 #Route::webhooks('api/xcelerateudi');
 
 Route::get('/addvaccine', function () {
     return view('addvaccine');
 });
+
+
