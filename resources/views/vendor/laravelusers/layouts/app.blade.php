b1b1812e667fea23f1bbff618903bf04929080ae
olsonhost <erik@olson.host>
2021-01-23 16:50:09 +0000

    server erik

--- resources/views/vendor/laravelusers/layouts/app.blade.php
+++ resources/views/vendor/laravelusers/layouts/app.blade.php
@@ -31,8 +31,8 @@
         <div id="app">
             <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
                 <div class="container">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                    <a class="navbar-brand small-logo" href="{{ url('/') }}">
+                        <img src="logo.jpg" style="height:48px;"/>
                     </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
