6277119e33bd6983d99e0791de2097bf9aae92eb
olsonhost <erik.olson@trackmysolutions.us>
2021-02-04 14:34:26 -0500

    wip

--- resources/views/app/page_modals.blade.php
+++ resources/views/app/page_modals.blade.php
@@ -56,7 +56,55 @@
 
             <!-- Eventually make this the CDN/Bucket Cover Page -->
 
-            <span id='cover-continue' onclick="$('.cover-page-modal').hide();">Continue</span>
+            <h1>Affirmation Statement</h1>
+            <div>
+                I affirm that I work with low incidence students and I meet one the of the 1A criteria listed below
+            </div>
+
+            <div>
+                <ul>
+                    <li>Health care personnel including, but not limited to:</li>
+                    <ul>
+                        <li>Emergency medical service personnel</li>
+                        <li>Nurses</li>
+                        <li>Physicians</li>
+                        <li>Dentists</li>
+                        <li>Dental hygenists</li>
+                        <li>Chiropractors</li>
+                        <li>Therapists</li>
+                        <li>Phlebotomists</li>
+                        <li>Pharmacists</li>
+                        <li>Technicians</li>
+                        <li>Pharamacy technicians</li>
+                        <li>Health professions students and trainees</li>
+                        <li>Direct support professionals</li>
+                        <li>Clinical personnel in school settings or correctional facilities</li>
+                        <li>Contractual HCP not directly employed by the health care facility</li>
+                        <li>Persons not directly involved in patient care but potentially exposed to infectious material that can transmit disease among or from health care personnel and patients</li>
+                        <li>Persons ages 65 and older</li>
+                        <li>Persons ages 16-64 with high risk conditions</li>
+                    </ul>
+                    <li>Cancer</li>
+                    <li>COPD</li>
+                    <li>Down Syndrome</li>
+                    <li>Heart conditions, such as heart failure, coronary artery disease, or cardiomyopathies</li>
+                    <li>Immunocompromised state (weakened immune system) from solid organ transplant or from blood or bone marrow transplant, immune deficiencies , HIV, use of corticosteroids, or use of other immune weakening medicines</li>
+                    <li>Obesity (BMI of 30 kg/m2 or higher but &#62 40 kg/m2)</li>
+                    <li>Severe obesity (BMI &#8805 40 kg/m2)</li>
+                    <li>Pregnancy</li>
+                    <li>Sickle cell disease</li>
+                    <li>Smoking</li>
+                    <li>Type 2 diabetes mellitus</li>
+                </ul>
+            </div>
+
+            <br>
+
+            <div style="text-align:center;">
+                <span class="btn btn-primary">I can affirm</span> <span class="btn btn-primary">I cannot affirm</span>
+            </div>
+
+{{--            <span id='cover-continue' onclick="$('.cover-page-modal').hide();">Continue</span>--}}
 
         </div>
 
